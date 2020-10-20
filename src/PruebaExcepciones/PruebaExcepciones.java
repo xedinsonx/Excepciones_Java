a88360bca801ffefa434f4f44a117a798c2b0891
xedinsonx <xedinsonx2@gmail.com>
2020-10-20 17:12:28 -0500

    Excepcion_Java

---
+++ src/PruebaExcepciones/PruebaExcepciones.java
@@ -0,0 +1,19 @@
+
+package PruebaExcepciones;
+
+import java.util.Scanner;
+
+
+public class PruebaExcepciones {
+    
+    public static void main(String[] args){
+       
+        Scanner entrada = new Scanner (System.in);
+        
+        System.out.println("Digite un numero: ");
+        int numero = entrada.nextInt();
+        
+        System.out.println(numero);
+    }
+    
+}
